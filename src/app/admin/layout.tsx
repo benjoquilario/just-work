5e4b239d7d63888c703d28cede804349c9629232
benjoquilario <benjoquilario@gmail.com>
2023-10-13 20:04:38 +0800

    fix: notfounc when accessing admin

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,4 @@
 import Container from "@/components/shared/container"
-import { getSession } from "@/lib/session"
-import { notFound } from "next/navigation"
 import React from "react"
 
 export default async function Layout({
@@ -8,9 +6,6 @@ export default async function Layout({
 }: {
   children: React.ReactNode
 }) {
-  const session = await getSession()
-
-  if (!session) notFound()
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
